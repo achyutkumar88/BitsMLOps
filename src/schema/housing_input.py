2ad2f25b3e62be8f06b58b741870f7bad21524be
achyutdwivedi <achyutkumar88@gmail.com>
2025-08-10 05:07:08 +0530

    Test

--- src/schema/housing_input.py
+++ src/schema/housing_input.py
@@ -3,12 +3,15 @@
 from pydantic import BaseModel, Field, model_validator
 from typing import List
 
+
 class HousingInput(BaseModel):
-    features: List[List[float]] = Field(..., description="List of samples, each with 8 float features")
+    desc= "List of samples, each with 8 float features"
+    features: List[List[float]] = Field(..., description=desc)
 
     @model_validator(mode='before')
     def check_feature_shape(cls, values):
         features = values.get('features')
         if not all(len(feature) == 8 for feature in features):
-            raise ValueError("Each feature list must contain exactly 8 float values.")
+            failure_Reason = "Each feature list must contain exactly 8 float values."
+            raise ValueError(failure_Reason)
         return values
