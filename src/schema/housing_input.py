022d8fc32401e0a82d05a9067d17d50b08a690a5
achyutdwivedi <achyutkumar88@gmail.com>
2025-08-10 05:08:47 +0530

    Test

--- src/schema/housing_input.py
+++ src/schema/housing_input.py
@@ -5,13 +5,13 @@ from typing import List
 
 
 class HousingInput(BaseModel):
-    desc= "List of samples, each with 8 float features"
+    desc = "List of samples, each with 8 float features"
     features: List[List[float]] = Field(..., description=desc)
 
     @model_validator(mode='before')
     def check_feature_shape(cls, values):
         features = values.get('features')
         if not all(len(feature) == 8 for feature in features):
-            failure_Reason = "Each feature list must contain exactly 8 float values."
+            failure_Reason = "Each feature list must contain 8 float values"
             raise ValueError(failure_Reason)
         return values
