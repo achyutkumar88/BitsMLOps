7fb0cb6748cc495f586181cb0373e63a5a0afc48
achyutdwivedi <achyutkumar88@gmail.com>
2025-08-10 06:18:12 +0530

    Test

--- src/test/test_predict_price.py
+++ src/test/test_predict_price.py
@@ -1,22 +1,51 @@
 from unittest.mock import patch, MagicMock
+
+
+# Patch mlflow.pyfunc.load_model before app imports it
+patcher = patch("mlflow.pyfunc.load_model")
+mock_load_model = patcher.start()
+mock_model = MagicMock()
+mock_model.predict.return_value = [123.45]
+mock_load_model.return_value = mock_model
+
+
 import sys
 import os
-
-# Insert project root in sys.path for imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-with patch("mlflow.pyfunc.load_model") as mock_load_model:
-    mock_model = MagicMock()
-    mock_model.predict.return_value = [123.45]
-    mock_load_model.return_value = mock_model
-
-    import app  # import app while patch is active
-
+from app import app
 from fastapi.testclient import TestClient
 
-client = TestClient(app.app)
+
+# Add src directory to sys.path for imports
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..",
+        )
+    ),
+)
+
+
+client = TestClient(app)
+
+def teardown_module(module):
+    patcher.stop()
+
+
+predict_path = "src.app.model.predict"
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    msg = "California Housing Prediction API is running."
+    assert response.json() == {"message": msg}
+
 
 def test_predict():
-    response = client.post("/predict", json={"features": [[1,2,3,4,5,6,7,8]]})
+    response = client.post(
+    "/predict", json={"features": [[1, 2, 3, 4, 5, 6, 7, 8]]}
+    )
     assert response.status_code == 200
     assert "predictions" in response.json()
